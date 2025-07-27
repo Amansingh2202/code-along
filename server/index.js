cfd2cffeec4468cc109e1fffec542b5141ce6365
Amansingh2202 <789singhaman@gmail.com>
2025-07-27 23:59:55 +0530

    Your custom changes to index.js

--- server/index.js
+++ server/index.js
@@ -76,5 +76,5 @@ socket.on("disconnecting", () => {
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
